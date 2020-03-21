dd33b4823063cb93bedb411dcdb5e990bd30c1a7
fl <610246763@qq.com>
2020-03-21 18:45:55 +0800

    second

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -3,10 +3,10 @@ from flask_cors import *
 
 app = Flask(__name__)
 
-CORS(app,supports_credentials=True)
+# CORS(app,supports_credentials=True)
 
 # r'/*' 是通配符，让本服务器所有的URL 都允许跨域请求
-#CORS(app, resources=r'/*')
+CORS(app, resources=r'/*')
 
 
 from app import routes
